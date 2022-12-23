d74312a5cf6a31cdf90f2f448e63639a40e6dcf6
Elisa <handlessdude@gmail.com>
2022-12-23 22:09:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def print_fact(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_fact(5)
